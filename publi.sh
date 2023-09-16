02605142244eea2145031d0c137d39e2ef87fda0
Albert Mata <hello@albertmata.net>
2023-09-16 07:31:28 +0200

    Personal website updated

--- publi.sh
+++ publi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git add .
-git commit -m "(Note) $1"
+git commit -m "Personal website updated"
 git push origin master
 cd _site
 git checkout CNAME
 git add .
-git commit -m "(Note) $1"
+git commit -m "Personal website updated"
 git push origin master
 cd ..
